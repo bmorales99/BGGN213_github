ab324f51f960527c123ad5155cad38635b90c69f
Bobbie Morales <bobbieeeemorales@Bobbie-MacBook-Air.local>
2025-12-05 14:42:14 -0800

    push files

---
+++ class04/class04.r
@@ -0,0 +1,16 @@
+#' ---
+#' title: "Class lab 4"
+#' author: "Bobbie Morales PID A15443382"
+#' date: "10/10/25"
+#' ---
+
+# This is my first R script
+
+
+x <- 1:50
+plot(x)
+plot(x, col='blue')
+plot(x, sin(x), typ = 'l', col="red", lwd=3)
+log(10)
+? log
+log(10, base=10)
